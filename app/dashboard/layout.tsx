43c8f0848d9b6a04ddcc15679f23b2961b58918f
Bobby Aleem <bobbybeatmaker@hotmail.com>
2024-12-06 20:50:13 +0500

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,19 @@
 import DashboardProviders from "@/components/providers/dashboard-providers";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import { FC, ReactNode } from "react";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
-const Layout: FC<LayoutProps> = ({ children }) => {
+const Layout: FC<LayoutProps> = async ({ children }) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
   return <DashboardProviders>{children}</DashboardProviders>;
 };
 
